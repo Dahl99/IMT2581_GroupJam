d52f1d95b820b3c83ade86fcea4a02de60e5a68b
Kacper Lewandowski <kacperl@stud.ntnu.no>
2021-02-20 17:36:26 +0100

    Comments about process of damage, and ability to hurt enemy

--- source/game_manager/game_manager.gd
+++ source/game_manager/game_manager.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal deal_damage
+
+var evade_damage : bool = true
 
 func _ready():
 	conecc_enemy()
@@ -9,6 +12,26 @@ func conecc_enemy():
 	for child in get_children():
 		if child.name == "Enemy":
 			child.connect("attack", self, "attack_by_enemy")
+			child.connect("prepare_attack", self, "prepare_attack")
 
+# Is run when the preparation attack signal is sent.
+# Does not deal damage by itself, but simply enables
+# the player to take damage if he doesn't evade
+func prepare_attack():
+	evade_damage = false
+
+
+# Is supposed to be run when player evades 
+func evade_attack():
+	evade_damage = true
+	pass
+
+# Is run when the attack signal is sent. 
+# Will deal damage to player if not evaded
 func attack_by_enemy():
-	print ("Hello")
+	if evade_damage:
+		# No damage taken, the player must use dodge though.
+		pass
+	else:
+		# Player takes damage
+		pass
