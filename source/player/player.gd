8b95ea54b06fdabb49cf6964503af03fca4f6960
Daniel Dahl <danieldahl99@gmail.com>
2021-02-20 18:03:36 +0100

    Added comments to player script

--- source/player/player.gd
+++ source/player/player.gd
@@ -1,25 +1,31 @@
 extends KinematicBody2D
 
+# Varables used to keep track of currently used buttons
+var _keys = ["A", "X", "Space", "P", "8", "B"]
+
 var _attack_btn: String
 var _dodge_btn: String
 
-var _health = 10
+var _max_health = 10	# Keeps track of player max health
+var _health = 10		# Keeps track of player health
 
-var _keys = ["A", "X", "Space", "P", "8", "B"]
-
+# When player object is created,
+# the keys will be shuffled
+# and a random attack and dodge
+# key will be chosen
 func _ready():
 	_keys.shuffle()
 	_attack_btn = _keys.front()
 	_dodge_btn = _keys.back()
 
 func _physics_process(_delta):
-	
-	if Input.is_action_just_pressed(_attack_btn):
-		_attack()
-		_attack_btn = _get_random_unused_key(_attack_btn, _dodge_btn)
-	elif Input.is_action_just_pressed(_dodge_btn):
-		_dodge()
-		_dodge_btn = _get_random_unused_key(_dodge_btn, _attack_btn)
+	if Input.is_action_just_pressed(_attack_btn):						# Checking if attack button is pressed
+		_attack()														# Execeuting attack
+		_attack_btn = _get_random_unused_key(_attack_btn, _dodge_btn)	# Assigning new key for attack action
+
+	elif Input.is_action_just_pressed(_dodge_btn):						# Checking if dodge button is pressed
+		_dodge()														# Executing dodge
+		_dodge_btn = _get_random_unused_key(_dodge_btn, _attack_btn)	# Assigning new key for dodge action
 
 func _attack():
 	pass
@@ -30,7 +36,10 @@ func _dodge():
 func _hurt(_dmg_taken: int):
 	_health -= _dmg_taken
 
-func _get_random_unused_key(_previous_key: String, _used_key: String):
+# Gets a randomly chosen new key
+# that's not equal to previously used key
+# or another currently in-use key
+func _get_random_unused_key(_previous_key: String, _used_key: String) -> String:
 	var _new_key: String
 	_keys.shuffle()
 	_new_key = _keys.front()
