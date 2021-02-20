d4e06fd47c82839002583ac64bbb6626fce0e3ee
Kacper Lewandowski <kacperl@stud.ntnu.no>
2021-02-20 16:16:49 +0100

    Indicator when the enemy is going to attack

--- source/enemy/enemy.gd
+++ source/enemy/enemy.gd
@@ -2,15 +2,29 @@ extends Node2D
 
 
 signal attack
+signal prepare_attack
 
 onready var attack_timer = $AttackTimer
+onready var prepare_timer = $AttackPrepareTimer
 onready var animation_player = $AnimationPlayer
 
-func start_timer():
-	var time = randi() % 5 + 3
-	attack_timer.start(time)
+func start_attack_timer():
+	attack_timer.start(1.3)
+
+func start_prepare_timer():
+	var time = randi() % 4 + 3
+	prepare_timer.start(time)
+
+func _ready():
+	start_prepare_timer()
 
 
 func _on_AttackTimer_timeout():
 	animation_player.play("Attack")
 	emit_signal("attack")
+
+func _on_AttackPrepareTimer_timeout():
+	animation_player.play("Prepare Attack")
+	emit_signal("prepare_attack")
+	start_attack_timer()
+	
--- source/game_manager/game_manager.gd
+++ source/game_manager/game_manager.gd
@@ -2,4 +2,13 @@ extends Node2D
 
 
 func _ready():
+	conecc_enemy()
 	pass
+
+func conecc_enemy():
+	for child in get_children():
+		if child.name == "Enemy":
+			child.connect("attack", self, "attack_by_enemy")
+
+func attack_by_enemy():
+	print ("Hello")
