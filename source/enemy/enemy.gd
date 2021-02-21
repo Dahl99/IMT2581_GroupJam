4c31196e64b3b7cef1c5b8d055721cfa91cc14ea
Kacper Lewandowski <kacperl@stud.ntnu.no>
2021-02-21 13:35:43 +0100

    Pop-up when game is finished

--- source/enemy/enemy.gd
+++ source/enemy/enemy.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 signal attack
 signal prepare_attack
+signal enemy_died
 
 onready var attack_timer = $AttackTimer
 onready var prepare_timer = $AttackPrepareTimer
@@ -14,7 +15,7 @@ export (int) var health = 5
 # that the enemy is about to attack. The attacking happens in 
 # start_attack_timer 
 func start_prepare_timer():
-	var time = randi() % 4 + 3
+	var time = randi() % 4 + 1
 	prepare_timer.start(time)
 
 # The attack happens always after 1.3 seconds after the preparation
@@ -29,12 +30,16 @@ func _die():
 	attack_timer.stop()
 	prepare_timer.stop()
 	
+	emit_signal("enemy_died")
+	
 	animation_player.clear_queue()
 	animation_player.play("Die")
 
 func _respawn():
 	health = max_health
 	animation_player.play("Respawn")
+	start_prepare_timer()
+	
 
 func _on_take_damage():
 	health = health - 1
@@ -47,6 +52,7 @@ func _on_take_damage():
 func _on_AttackTimer_timeout():
 	animation_player.play("Attack")
 	emit_signal("attack")
+	start_prepare_timer()
 
 func _on_AttackPrepareTimer_timeout():
 	animation_player.stop()
--- source/game_manager/game_manager.gd
+++ source/game_manager/game_manager.gd
@@ -3,6 +3,7 @@ extends Node2D
 # Preloading
 onready var player = preload("res://source/player/player.tscn").instance()
 onready var enemy = preload("res://source/enemy/enemy.tscn").instance()
+onready var pop_up = preload("res://source/menus/pop-up.tscn").instance()
 
 # Signals
 signal enemy_deal_damage
@@ -10,6 +11,7 @@ signal player_deal_damage
 
 # Variables
 var evade_damage : bool = true
+var enemies_killed : int = 0
 
 func _ready():
 	_conecc_enemy()
@@ -18,14 +20,15 @@ func _ready():
 	randomize() 			# Randomizes rng seed
 
 	add_child(enemy)
-	enemy.position = Vector2(216, 57)
+	enemy.position = Vector2(179, 768 - 200)
 
 	self.add_child(player)	# Adding player to scene
-	player.position = Vector2(1024/2, 768/2)
+	player.position = Vector2(652, 768 - 200)
 
 func _conecc_enemy():
 	enemy.connect("attack", self, "_attack_by_enemy")
 	enemy.connect("prepare_attack", self, "_prepare_attack")
+	enemy.connect("enemy_died", self, "_on_enemy_died")
 
 
 func _conecc_player():
@@ -55,6 +58,14 @@ func _attack_by_enemy():
 		print ("hello")
 		emit_signal("enemy_deal_damage")
 
+
+func _on_enemy_died():
+	enemies_killed += 1
+	if enemies_killed >= 1:
+		add_child(pop_up)
+		pop_up.visible = true
+		pop_up.rect_position = Vector2(get_viewport().size.x/2, get_viewport().size.y/2)
+ 
 func _on_Button_button_down():
 	_player_attack()
 	pass # Replace with function body.
