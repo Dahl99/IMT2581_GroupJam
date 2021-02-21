419e421670022db7956e43de1bc8108c62885645
Daniel Dahl <danieldahl99@gmail.com>
2021-02-21 10:59:03 +0100

    Implemented signals from player to dodge and attack

--- source/game_manager/game_manager.gd
+++ source/game_manager/game_manager.gd
@@ -4,23 +4,28 @@ extends Node2D
 onready var player = preload("res://source/player/player.tscn").instance()
 
 # Signals
-signal deal_damage
+signal player_deal_damage
 
 # Variables
 var evade_damage : bool = true
 
 func _ready():
-	conecc_enemy()
+	_conecc_enemy()
+	_conecc_player()
 
 	randomize() 			# Randomizes rng seed
 
 	self.add_child(player)	# Adding player to scene
 
-func conecc_enemy():
+func _conecc_enemy():
 	for child in get_children():
 		if child.name == "Enemy":
 			child.connect("attack", self, "attack_by_enemy")
 			child.connect("prepare_attack", self, "prepare_attack")
+
+func _conecc_player():
+	player.connect("dodge", self, "player_evade_attack")
+	player.connect("attack", self, "player_attack")
 
 # Is run when the preparation attack signal is sent.
 # Does not deal damage by itself, but simply enables
@@ -30,9 +35,13 @@ func prepare_attack():
 
 
 # Is supposed to be run when player evades 
-func evade_attack():
+func player_evade_attack():
 	evade_damage = true
 	pass
+
+# Function to be executed when player presses attack button
+func player_attack():
+	emit_signal("player_deal_damage")
 
 # Is run when the attack signal is sent. 
 # Will deal damage to player if not evaded
